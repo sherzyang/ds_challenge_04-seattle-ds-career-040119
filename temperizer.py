af360519e0209772faa3d6774104408a4e21e7ab
Sherry <sherry.yang33@gmail.com>
2019-04-04 09:31:35 -0700

    Add functions

--- temperizer.py
+++ temperizer.py
@@ -9,4 +9,25 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    temperature_f = (temperature_c*(9/5))+32
+    return temperature_c
+
+def convert_c_to_k(temperature_c): 
+    """Convert Celsius to Kelvin."""
+    temperature_k = temperature_c + 273.15
+    return temperature_k
+
+def convert_f_to_k(temperature_f):
+    """Convert Farienheit to Kelvin."""
+    temperature_k = (temperature_f + 459.67)* (5/9)
+    return temperature_k
+    
+def convert_k_to_c(temperature_k):
+    """Convert Kelvin to Celcius."""
+    temperature_c = temperature_k - 273.15
+    return temperature_c
+
+def convert_k_to_f(temperature_k):
+    """Convert Kelvin to Farienheit."""
+    temperature_f = temperature_k * (9/5) - 459.67
+    return temperature_f
